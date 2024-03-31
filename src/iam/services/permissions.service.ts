01542ab8efe9c163aaf91fe81a2756f8aa98ee24
Issam Mezgueldi <issammez44@gmail.com>
2024-03-31 17:22:34 +0000

    finilize permissions service

--- src/iam/services/permissions.service.ts
+++ src/iam/services/permissions.service.ts
@@ -7,13 +7,29 @@ import { UpdatePermissionDto } from '../dto/update-permission.dto';
 export class PermissionsService {
   constructor(@Inject('IAM_SERVICE') private readonly iamClient: ClientProxy) {}
 
-  async create(createPermissionDto: CreatePermissionDto) {}
+  async create(createPermissionDto: CreatePermissionDto) {
+    return this.iamClient.send(
+      { cmd: 'create_permission' },
+      createPermissionDto,
+    );
+  }
 
-  async findAll() {}
+  async findAll() {
+    return this.iamClient.send({ cmd: 'get_permissions' }, {});
+  }
 
-  async findOne(id: string) {}
+  async findOne(id: string) {
+    return this.iamClient.send({ cmd: 'get_permission' }, id);
+  }
 
-  async update(id: string, updatePermissionDto: UpdatePermissionDto) {}
+  async update(id: string, updatePermissionDto: UpdatePermissionDto) {
+    return this.iamClient.send(
+      { cmd: 'update_permission' },
+      { id, ...updatePermissionDto },
+    );
+  }
 
-  async remove(id: string) {}
+  async remove(id: string) {
+    return this.iamClient.send({ cmd: 'delete_permission' }, id);
+  }
 }

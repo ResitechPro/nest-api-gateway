0feaa7dccf579d65011788b797c292028eee78bb
Issam Mezgueldi <issammez44@gmail.com>
2024-04-01 16:40:16 +0000

    set 'api' as global prefix and enable cors for frontend origin

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.use(cookieParser());
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: configService.get('CORS_ORIGIN'),
+    credentials: true,
+  });
   await app.listen(configService.get('HTTP_PORT'));
 }
 bootstrap();
